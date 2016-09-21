6c3cbbe9207030994e6f7d1b383466104811e045
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-09-21 11:56:42 +0200

    add protective request before cluster operation calls

--- cluster-down.sh
+++ cluster-down.sh
@@ -4,6 +4,12 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 set -e
 
+echo; read -p "do you realy want to shutdown the current jenkins-cluster | are you sure (y/n) ?" -n 1 -r; echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 2
+fi
+
 ./kube-down.sh
 
 echo -e "\n"
--- cluster-up.sh
+++ cluster-up.sh
@@ -4,6 +4,12 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 set -e
 
+echo; read -p "do you realy want to start the current jenkins-cluster | are you sure (y/n) ?" -n 1 -r; echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 2
+fi
+
 echo -e "\n"
 echo -e "-- ⚑ -- | → KUBERNETES :: start main cluster (1 node)"
 echo -e "        | ⇢ $(uname -nm)"
